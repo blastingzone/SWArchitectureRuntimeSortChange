e2d3c53430b4afb59f6673d370161b4956659d6c
blastingzone <blastingzone@nhnnext.org>
2014-03-17 23:15:32 +0900

    함수 정리

--- SWArchitectureRuntimeSortChange/src/caller/Main.java
+++ SWArchitectureRuntimeSortChange/src/caller/Main.java
@@ -10,18 +10,20 @@ import config.XMLParser;
 
 public class Main {
 	
+	static Isort iSort = null;
+	
 	static int test[] = {0, 10, 9, 25, 7, 6, 3, 8, 13, 15};
 	
 	public static void main(String[] args) {
 		Serializer serializer = new Persister();
 		File file = new File("Configuration.xml");
-
-		Isort iSort = null;
+		
 		try {
 			XMLParser parser = serializer.read(XMLParser.class, file);
 			String sortType = "sortAlgorithms." + parser.GetSortType();
 			// print Sort Type
 			System.out.println("Sort Type : " + sortType);
+			// create sort by Configuration.xml
 			iSort = (Isort) Class.forName( sortType ).newInstance();
 		} catch (Exception e) {
 			 System.err.println("Cannot Create Sort Class");
@@ -29,17 +31,18 @@ public class Main {
 		
 		// before sorting
 		System.out.println("Original Data");
-		for (int iter : test) {
-			System.out.printf("%d ", iter);
-		}
-		System.out.printf("\n");
+		printDataArray(test);
 
 		// do sorting
 		int[] sortedData = iSort.sort(test);
 		
 		// after sorting
 		System.out.println("Sorting Complete");
-		for (int iter : sortedData) {
+		printDataArray(sortedData);
+	}
+	
+	private static void printDataArray(int[] data) {
+		for (int iter : data) {
 			System.out.printf("%d ", iter);
 		}
 		System.out.printf("\n");
